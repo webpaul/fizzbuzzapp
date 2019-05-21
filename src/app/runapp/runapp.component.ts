1ec9e449a0bdeee30b19b21616bcd9d73dde3c6b
EricV-Dev <eric_j_villarreal@Yahoo.com>
2019-05-21 15:22:49 -0500

    new commit

--- src/app/runapp/runapp.component.ts
+++ src/app/runapp/runapp.component.ts
@@ -6,39 +6,70 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./runapp.component.css"]
 })
 export class RunappComponent implements OnInit {
-  fizzResult = "";
+  fizzResult = [];
 
   constructor() {}
 
   ngOnInit() {}
 
-  fizzBuzz(i) {
-    let retVal = "";
-    let notDivBy3 = false;
-    let notDivBy5 = false;
+  fizzBuzz() {
+    let x = [];
+    for (let i = 1; i <= 36; i++) {
+      let retVal = "";
+      let notDivBy3 = false;
+      let notDivBy5 = false;
 
-    if (i % 3 == 0) {
-      retVal += "Fizz";
-      notDivBy3 = true;
-    }
+      if (i % 3 == 0) {
+        retVal += "Fizz";
+        notDivBy3 = true;
+        x.push(retVal);
+      }
 
-    if (i % 5 == 0) {
-      retVal += " Buzz";
-      retVal = retVal.trim();
-      notDivBy5 = true;
-    }
+      if (i % 5 == 0) {
+        retVal += " Buzz";
+        retVal = retVal.trim();
+        notDivBy5 = true;
+        x.push(retVal);
+      }
 
-    if (notDivBy3 == false && notDivBy5 == false) {
-      return i;
+      if (notDivBy3 == false && notDivBy5 == false) {
+        x.push(i);
+      }
     }
-    return retVal;
+    return x;
   }
 
   outputFizzBuzz() {
-    for (let i = 1; i <= 36; i++) {
-      let fResults = this.fizzBuzz(i);
-      this.fizzResult += fResults + " ";
-      console.log(fResults);
-    }
+    this.fizzResult = this.fizzBuzz();
   }
 }
+
+// fizzBuzz(i) {
+//   let retVal = "";
+//   let notDivBy3 = false;
+//   let notDivBy5 = false;
+
+//   if (i % 3 == 0) {
+//     retVal += "Fizz";
+//     notDivBy3 = true;
+//   }
+
+//   if (i % 5 == 0) {
+//     retVal += " Buzz";
+//     retVal = retVal.trim();
+//     notDivBy5 = true;
+//   }
+
+//   if (notDivBy3 == false && notDivBy5 == false) {
+//     return i;
+//   }
+//   return retVal;
+// }
+
+// outputFizzBuzz() {
+//   for (let i = 1; i <= 36; i++) {
+//     let fResults = this.fizzBuzz(i);
+//     this.fizzResult += fResults + " ";
+//     console.log(fResults);
+//   }
+// }
