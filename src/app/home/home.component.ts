b4fa013bf7f9d94921416ba3b656cd0ce78531fc
EricV-Dev <eric_j_villarreal@Yahoo.com>
2019-05-20 22:07:36 -0500

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { EventEmitterService } from "../event-emitter.service";
 
 @Component({
@@ -10,40 +10,12 @@ export class HomeComponent implements OnInit {
   constructor(private eventEmitterService: EventEmitterService) {}
 
   ngOnInit() {
-    if (this.eventEmitterService.subsVar == undefined) {
-      this.eventEmitterService.subsVar = this.eventEmitterService.invokeFirstComponentFunction.subscribe(
-        (name: string) => {
-          this.outputFizzBuzz();
-        }
-      );
-    }
-  }
-
-  fizzBuzz(i) {
-    let retVal = "";
-    let notDivBy3 = false;
-    let notDivBy5 = false;
-
-    if (i % 3 == 0) {
-      retVal += "Fizz";
-      notDivBy3 = true;
-    }
-
-    if (i % 5 == 0) {
-      retVal += " Buzz";
-      retVal = retVal.trim();
-      notDivBy5 = true;
-    }
-
-    if (notDivBy3 == false && notDivBy5 == false) {
-      return i;
-    }
-    return retVal;
-  }
-
-  outputFizzBuzz() {
-    for (let i = 1; i <= 36; i++) {
-      console.log(this.fizzBuzz(i));
-    }
+    // if (this.eventEmitterService.subsVar == undefined) {
+    //   this.eventEmitterService.subsVar = this.eventEmitterService.invokeFirstComponentFunction.subscribe(
+    //     (name: string) => {
+    //       this.outputFizzBuzz();
+    //     }
+    //   );
+    // }
   }
 }
--- src/app/runapp/runapp.component.ts
+++ src/app/runapp/runapp.component.ts
@@ -6,7 +6,38 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./runapp.component.css"]
 })
 export class RunappComponent implements OnInit {
+  fizzResult = "this.fizzResult";
+
   constructor() {}
 
   ngOnInit() {}
+
+  fizzBuzz(i) {
+    let retVal = "";
+    let notDivBy3 = false;
+    let notDivBy5 = false;
+
+    if (i % 3 == 0) {
+      retVal += "Fizz";
+      notDivBy3 = true;
+    }
+
+    if (i % 5 == 0) {
+      retVal += " Buzz";
+      retVal = retVal.trim();
+      notDivBy5 = true;
+    }
+
+    if (notDivBy3 == false && notDivBy5 == false) {
+      return i;
+    }
+    return retVal;
+  }
+
+  outputFizzBuzz() {
+    for (let i = 1; i <= 36; i++) {
+      let fizzResult = [this.fizzBuzz(i)];
+      console.log(fizzResult);
+    }
+  }
 }
